89137a70368d3f5ebf6ba65886f8d621dfa846e6
sviridovdev <sviridov.dev@gmail.com>
2019-06-03 21:44:48 +0300

    refactoring

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Button.less'
 
+interface ButtonProps {
+    clickHandler: any;
+    text: string;
+}
+
 /**
  * Реализует компонент кнопки
  * @param props
  * @returns {any}
  * @constructor
  */
-export default function Button(props: any): any {
+const Button: React.StatelessComponent<ButtonProps> = (props) => {
     return (
         <button onClick={props.clickHandler}>{props.text}</button>
     );
-}
+};
+
+export default Button;
--- src/RadioSortAlg.tsx
+++ src/RadioSortAlg.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
+interface RadioProps {
+    value: number;
+    onchange: any;
+    checked: boolean;
+    label: string;
+}
+
 /**
  * Реализует компонент радиокнопки выбора алгоритма сортировки
  * @param props
  * @returns {any}
  * @constructor
  */
-export default function RadioSortAlg(props: any): any {
+const RadioSortAlg: React.StatelessComponent<RadioProps> = (props) => {
     return (
         <label>
             <input type="radio" name="sortalg" value={props.value} onChange={props.onchange} checked={props.checked}/>
             {props.label}
         </label>
     );
-}
+};
+
+export default RadioSortAlg;
